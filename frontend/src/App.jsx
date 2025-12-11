77d9f74123bb9db5036a08556b6521c4d2670df3
HimanshiSh03 <116914206+HimanshiSh03@users.noreply.github.com>
2025-12-11 20:38:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Kanban from "./Kanban";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
@@ -17,74 +18,87 @@ function App() {
   localStorage.setItem("isLoggedIn", "true");
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Router>
+      <div className="min-h-screen bg-gray-100">
 
-        <Route
-          path="/contributors"
-          element={
-            <ProtectedRoute>
-              <Contributors />
-            </ProtectedRoute>
-          }
-        />
+        {/* Navbar */}
+        <nav className="shadow-md bg-white py-4 px-6 md:px-10 flex items-center justify-between">
+          <div className="text-2xl font-bold text-blue-600">
+            <Link to="/">LearnSpace</Link>
+          </div>
 
-        <Route
-          path="/owner"
-          element={
-            <ProtectedRoute>
-              <About />
-            </ProtectedRoute>
-          }
-        />
+          <div className="flex flex-wrap gap-6 md:gap-8 text-lg">
+            <Link to="/">Home</Link>
+            <Link to="/kanban">Kanban</Link>
+            <Link to="/books">BookSpace</Link>
+            <Link to="/whiteboard">Whiteboard</Link>
+            <Link to="/contributors">Contributors</Link>
+            <Link to="/about">About</Link>
+          </div>
+        </nav>
 
-        <Route
-          path="/kanban"
-          element={
-            <ProtectedRoute>
-              <Kanban />
-            </ProtectedRoute>
-          }
-        />
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        <Route
-          path="/books"
-          element={
-            <ProtectedRoute>
-              <Books />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/contributors"
+            element={
+              <ProtectedRoute>
+                <Contributors />
+              </ProtectedRoute>
+            }
+          />
+          
+          <Route
+            path="/kanban"
+            element={
+              <ProtectedRoute>
+                <Kanban />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/book/:id"
-          element={
-            <ProtectedRoute>
-              <BookPreview />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/books"
+            element={
+              <ProtectedRoute>
+                <Books />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/whiteboard"
-          element={
-            <ProtectedRoute>
-              <Whiteboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
- <Route
-          path="/about"
-          element={
-            <ProtectedRoute>
-              <Whiteboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </div>
+          <Route
+            path="/book/:id"
+            element={
+              <ProtectedRoute>
+                <BookPreview />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/whiteboard"
+            element={
+              <ProtectedRoute>
+                <Whiteboard />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* FIXED: About route correctly placed inside <Routes> */}
+          <Route
+            path="/about"
+            element={
+              <ProtectedRoute>
+                <About />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
